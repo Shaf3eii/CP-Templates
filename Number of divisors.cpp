3395ec3049dd15d6f1c5b95c89731ef1a683319e
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2025-01-15 23:13:35 +0200

    Update Number of divisors.cpp

--- Number of divisors.cpp
+++ Number of divisors.cpp
@@ -2,12 +2,12 @@ int number_of_divisors(ll n) {
     int divisors = 0;
     
     // Iterate over all numbers from 1 to sqrt(n)
-    for (int i = 1; i < sqrt(n); i++) {
+    for (int i = 1; i * i < n; i++) {
         if (n % i == 0) {
             divisors += 2;  // i and n/i are divisors
         }
     }
 
     // Check if n is a perfect square, if so, subtract 1 from divisors
-    return divisors + (sqrt(n) == (int)sqrt(n));
+    return divisors + (i * i == n);
 }
