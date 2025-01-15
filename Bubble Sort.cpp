fd25f74aef5cc26d955dec4e83d5a115510136ac
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2025-01-15 22:27:27 +0200

    Update Bubble Sort.cpp

--- Bubble Sort.cpp
+++ Bubble Sort.cpp
@@ -1,23 +1,19 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 void bubbleSort(vector<int>& a) {
-    int swaps = 0;
+    int n = (int)a.size(); 
+    int swaps = 0;    // Counter for the number of swaps.
+    
+    // Bubble Sort implementation.
     for (int step = 1; step <= n - 1; step++) {
-        for (int i = 1; i < n; i++) {
+        for (int i = 0; i < n - step; i++) { // Adjusted condition to avoid out-of-bound access.
             if (a[i] > a[i + 1]) {
                 swap(a[i], a[i + 1]);
                 ++swaps;
             }
         }
     }
+    
+    // Output results.
     cout << "Array is sorted in " << swaps << " swaps.\n";
     cout << "First Element: " << a[0] << '\n';
     cout << "Last Element: " << a[n - 1] << '\n';
-}
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
 }
