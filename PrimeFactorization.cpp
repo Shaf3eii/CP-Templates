0e2d57ceb1b844f4d422c3fa51c9ad3a2c6eafad
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2025-05-12 23:43:26 +0300

    Update PrimeFactorization.cpp

--- PrimeFactorization.cpp
+++ PrimeFactorization.cpp
@@ -1,21 +1,15 @@
-vector<int> prime_factorization(ll n) {
-    vector<int> factors;  // Vector to store the factors
-    // Divide by 2 as long as it's divisible
-    while (n % 2 == 0) {
-        factors.push_back(2);  // 2 is a prime factor
-        n /= 2;  // Divide n by 2 to remove factor
-    }
-    // Iterate through odd numbers from 3 to sqrt(n)
-    for (int i = 3; i <= sqrt(n); i += 2) {
-        // Divide by i as long as it's divisible
-        while (n % i == 0) {
-            factors.push_back(i);  // i is a prime factor
-            n /= i;  // Divide n by i to remove factor
+vector<long long> primeFactors(long long x) {
+    vector<long long> factors;
+    for (int i = 2; i * i <= x; ++i) {
+        while (x % i == 0) {
+            factors.push_back(i);
+            x /= i;
         }
     }
-    // If n is greater than 2, it must be a prime factor
-    if (n > 2) {
-        factors.push_back(n);  // n is prime
+
+    if (x > 1) {
+        factors.push_back(x);
     }
-    return factors;  // Return the prime factors
+
+    return factors;
 }
