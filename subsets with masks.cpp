7ad3838bbd79a52f3d3b838400d0fbd658a8ef9f
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2025-05-16 17:01:02 +0300

    Create subsets with masks.cpp

---
+++ subsets with masks.cpp
@@ -0,0 +1,9 @@
+for(int mask = 0 ; mask < (1 << sz(s)) ; ++mask) {
+        int cur = 0;
+        for(int i = 0 ; i < sz(s); ++i) {
+            if(mask & (1 << i)) {
+                
+            }
+        }
+        // do operation
+    }
