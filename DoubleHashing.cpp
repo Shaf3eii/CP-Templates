0065c69e44eb151ae33d6cb4ef235e5993679628
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2025-05-25 00:19:53 +0300

    Update DoubleHashing.cpp

--- DoubleHashing.cpp
+++ DoubleHashing.cpp
@@ -1,68 +1,51 @@
-const int N = 1e6 + 5;
-int B1, B2, MOD1, MOD2;
-int pw1[N], pw2[N];
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-struct DoubleHash {
-private:
-  vector<pair<int, int>> prefix;
-  int n;
-
-  void hash_prefix(const string &str) {
-    int h1 = 0, h2 = 0;
-    prefix.clear();
-    prefix.reserve(str.size());
-    for (char i: str) {
-      h1 = (1LL * h1 * B1) % MOD1;
-      h1 = (h1 + i) % MOD1;
-      h2 = (1LL * h2 * B2) % MOD2;
-      h2 = (h2 + i) % MOD2;
-      prefix.emplace_back(h1, h2);
-    }
-  }
-
-public:
-  DoubleHash() {}
-
-  DoubleHash(const string &s) { build(s); }
-
-  void build(const string &s) {
-    assert(B1);
-    n = static_cast<long long>(s.size());
-    hash_prefix(s);
-  }
-
-  pair<int, int> getPrefix(int l, int r) {
-    auto ret = prefix[r];
-    int sz = r - l + 1;
-    l--;
-    if (l >= 0) {
-      ret.first -= 1LL * prefix[l].first * pw1[sz] % MOD1;
-      if (ret.first < 0)ret.first += MOD1;
-      ret.second -= 1LL * prefix[l].second * pw2[sz] % MOD2;
-      if (ret.second < 0)ret.second += MOD2;
-    }
-    return ret;
-  }
-};
-
-void build() {
-  if (B1)return;
-  random_device rd;
-  mt19937 mt(rd());
-  auto rnd = [&](int l, int r) { return uniform_int_distribution<int>(l, r)(mt); };
-  auto check = [](int x) {
-    for (int i = 2; i <= x / i; ++i) if (!(x % i)) return false;
-    return true;
-  };
-  B1 = rnd(100, 500);
-  B2 = rnd(100, 500);
-  MOD1 = rnd(2e8, 2e9);
-  MOD2 = rnd(2e8, 2e9);
-  while (!check(MOD1))MOD1--;
-  while (MOD1 == MOD2 or !check(MOD2))MOD2--;
-  pw1[0] = pw2[0] = 1;
-  for (int i = 1; i < N; ++i) {
-    pw1[i] = 1LL * pw1[i - 1] * B1 % MOD1;
-    pw2[i] = 1LL * pw2[i - 1] * B2 % MOD2;
-  }
+template<typename T>
+T rand(T l, T r) {
+    return uniform_int_distribution<T>(l, r)(rng);
 }
+
+struct Hash {
+private:
+    const int mod1 = 1e9 + 7, mod2 = 1e9 + 9;
+    int base1, base2;
+    int n;
+    vector<int> pw1, pw2, hash1, hash2;
+
+    vector<int> hash(string &s, vector<int> &pw, const int mod) {
+        vector<int> res(n);
+        res[0] = (s[0] - 'a' + 1);
+        for (int i = 1; i < n; i++) {
+            res[i] = 1LL * (s[i] - 'a' + 1) * pw[i] % mod + res[i - 1];
+            if (res[i] >= mod) res[i] -= mod;
+        }
+        return res;
+    }
+    
+    int query(vector<int> &hash, vector<int> &pw, int l, int r, const int mod) {
+        int res = hash[r];
+        if (l > 0) res -= hash[l - 1];
+        if (res < 0) res += mod;
+        res = 1LL * res * pw[n - l] % mod;
+        return res;
+    }
+public:
+    Hash(string &s) {
+        n = s.size();
+        base1 = rand((int) 1e6, (int) 1e8);
+        base2 = rand((int) 1e6, (int) 1e8);
+        pw1.resize(n + 1), pw2.resize(n + 1);
+        pw1[0] = pw2[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            pw1[i] = 1LL * base1 * pw1[i - 1] % mod1;
+            pw2[i] = 1LL * base2 * pw2[i - 1] % mod2;
+        }
+        hash1 = hash(s, pw1, mod1);
+        hash2 = hash(s, pw2, mod2);
+    }
+
+    long long get_hash(int l, int r) {
+        return 1LL * query(hash1, pw1, l, r, mod1) *
+               query(hash2, pw2, l, r, mod2);
+    }
+};
