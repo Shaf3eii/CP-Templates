3a31624ac45d81948ec3bc93de087b8970c7e761
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2025-05-25 00:19:31 +0300

    Update SingleHashing.cpp

--- SingleHashing.cpp
+++ SingleHashing.cpp
@@ -1,60 +1,47 @@
-const int N = 1e6 + 5;
-int B1, MOD1;
-int pw1[N];
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
-struct SingleHash {
-private:
-  vector<int> prefix;
-  int n;
-
-  void hash_prefix(const string &str) {
-    int h1 = 0;
-    prefix.clear();
-    prefix.reserve(str.size());
-    for (char i: str) {
-      h1 = (1LL * h1 * B1) % MOD1;
-      h1 = (h1 + i) % MOD1;
-      prefix.emplace_back(h1);
-    }
-  }
-
-public:
-  SingleHash() {}
-
-  SingleHash(const string &s) { build(s); }
-
-  void build(const string &s) {
-    assert(B1);
-    n = static_cast<long long>(s.size());
-    hash_prefix(s);
-  }
-
-  int getPrefix(int l, int r) {
-    auto ret = prefix[r];
-    int sz = r - l + 1;
-    l--;
-    if (l >= 0) {
-      ret -= 1LL * prefix[l] * pw1[sz] % MOD1;
-      if (ret < 0)ret += MOD1;
-    }
-    return ret;
-  }
-};
-
-void build() {
-  if (B1)return;
-  random_device rd;
-  mt19937 mt(rd());
-  auto rnd = [&](int l, int r) { return uniform_int_distribution<int>(l, r)(mt); };
-  auto check = [](int x) {
-    for (int i = 2; i <= x / i; ++i) if (!(x % i)) return false;
-    return true;
-  };
-  B1 = rnd(100, 500);
-  MOD1 = rnd(2e8, 2e9);
-  while (!check(MOD1))MOD1--;
-  pw1[0] = 1;
-  for (int i = 1; i < N; ++i) {
-    pw1[i] = 1LL * pw1[i - 1] * B1 % MOD1;
-  }
+template<typename T>
+T rand(T l, T r) {
+    return uniform_int_distribution<T>(l, r)(rng);
 }
+
+struct Hash {
+private:
+    const int mod1 = 1e9 + 7;
+    int base1;
+    int n;
+    vector<int> pw1, hash1;
+
+    vector<int> hash(string &s, vector<int> &pw, const int mod) {
+        vector<int> res(n);
+        res[0] = (s[0] - 'a' + 1);
+        for (int i = 1; i < n; i++) {
+            res[i] = 1LL * (s[i] - 'a' + 1) * pw[i] % mod + res[i - 1];
+            if (res[i] >= mod) res[i] -= mod;
+        }
+        return res;
+    }
+    
+    int query(vector<int> &hash, vector<int> &pw, int l, int r, const int mod) {
+        int res = hash[r];
+        if (l > 0) res -= hash[l - 1];
+        if (res < 0) res += mod;
+        res = 1LL * res * pw[n - l] % mod;
+        return res;
+    }
+public:
+    Hash(string &s) {
+        n = s.size();
+        base1 = rand((int) 1e6, (int) 1e8);
+        pw1.resize(n + 1);
+        pw1[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            pw1[i] = 1LL * base1 * pw1[i - 1] % mod1;
+        }
+        hash1 = hash(s, pw1, mod1);
+    }
+    
+    long long get_hash(int l, int r) {
+        return 1LL * query(hash1, pw1, l, r, mod1);
+    }
+};
