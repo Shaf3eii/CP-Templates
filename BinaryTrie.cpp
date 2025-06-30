0415fbdfc5c4f61aa4679785dd793ad26b44cb1f
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2025-07-01 01:13:12 +0300

    Create BinaryTrie.cpp

---
+++ BinaryTrie.cpp
@@ -0,0 +1,54 @@
+struct BinaryTrie {
+    struct Node {
+        Node* child[2];
+        int freq[2];
+        Node() {
+            child[0] = child[1] = nullptr;
+            freq[0] = freq[1] = 0;
+        }
+    };
+    Node* root;
+    BinaryTrie() {
+        root = new Node();
+        insert(0); // remove this if not needed
+    }
+    void insert(int x) {
+        Node* cur = root;
+        for (int i = 30; ~i; --i) {
+            bool idx = (x >> i) & 1;
+            if (cur->child[idx] == nullptr) {
+                cur->child[idx] = new Node();
+            }
+            cur->freq[idx]++;
+            cur = cur->child[idx];
+        }
+    }
+    void del(int x, int b, Node* cur) {
+        if (b == -1) return;
+        bool idx = (x >> b) & 1;
+        del(x, b - 1, cur->child[idx]);
+        cur->freq[idx]--;
+        if (cur->freq[idx] == 0) {
+            delete cur->child[idx];
+            cur->child[idx] = nullptr;
+        }
+    }
+    void delete_(int x) {
+        del(x, 30, root);
+    }
+
+    int query(int x) {
+        Node* cur = root;
+        int ret = 0;
+        for (int i = 30; ~i; --i) {
+            bool idx = (x >> i) & 1;
+            if (cur->child[idx ^ 1] != nullptr) {
+                cur = cur->child[idx ^ 1];
+                ret |= 1 << i;
+            } else {
+                cur = cur->child[idx];
+            }
+        }
+        return ret;
+    }
+};
