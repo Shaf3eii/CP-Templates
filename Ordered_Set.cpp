e513e95060e2251bea4bab0ea1fde35827a85bc1
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2025-08-06 15:21:59 +0300

    Create Ordered_Set.cpp

---
+++ Ordered_Set.cpp
@@ -0,0 +1,19 @@
+// Ordered Set 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+
+
+// Ordered MutliSet
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<>, rb_tree_tag,tree_order_statistics_node_update> // set
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
